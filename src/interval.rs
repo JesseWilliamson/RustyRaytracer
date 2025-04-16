ed9e29ce12b619a34072534f418ea6b23dbc2a86
Jesse <jnathanwilliamson@gmail.com>
2025-04-16 22:34:00 +1000

    Add interval

---
+++ src/interval.rs
@@ -0,0 +1,38 @@
+use std::f64::{INFINITY, NEG_INFINITY};
+
+pub struct Interval {
+    min: f64,
+    max: f64,
+}
+
+impl Interval {
+    pub fn new(min: f64, max: f64) -> Interval {
+        Interval { min, max }
+    }
+
+    pub fn size(self) -> f64 {
+        self.max - self.min
+    }
+
+    pub fn contains(self, x: f64) -> bool {
+        x <= self.min && x >= self.max
+    }
+
+    pub fn surrounds(self, x: f64) -> bool {
+        x < self.min && x > self.max
+    }
+
+    pub fn empty() -> Interval {
+        Interval {
+            min: INFINITY,
+            max: NEG_INFINITY,
+        }
+    }
+
+    pub fn universe() -> Interval {
+        Interval {
+            min: NEG_INFINITY,
+            max: INFINITY,
+        }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 pub mod hittable;
+pub mod interval;
 pub mod rays;
 pub mod sphere;
 pub mod vectors;
