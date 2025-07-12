e1b284839160ab308bca98f258d9fa45501cb581
Jesse Williamson <jnathanwilliamson@gmail.com>
2025-07-12 16:07:25 +1000

    Linear to gamma colour space

--- src/color.rs
+++ src/color.rs
@@ -6,13 +6,24 @@ pub type Color = Vec3;
 
 impl Color {
     pub fn write_color<W: Write>(&self, out: &mut W) -> Result<(), std::io::Error> {
-        let r = self.x();
-        let g = self.y();
-        let b = self.z();
+        let mut r = self.x();
+        let mut g = self.y();
+        let mut b = self.z();
+        r = Color::linear_to_gamma(r);
+        g = Color::linear_to_gamma(g);
+        b = Color::linear_to_gamma(b);
         let intensity = Interval::new(0.000, 0.999);
         let r_byte = (255.999 * intensity.clamp(r)) as i32;
         let g_byte = (255.999 * intensity.clamp(g)) as i32;
         let b_byte = (255.999 * intensity.clamp(b)) as i32;
         writeln!(out, "{} {} {}", r_byte, g_byte, b_byte)
+    }
+
+    pub fn linear_to_gamma(linear_component: f64) -> f64 {
+        if linear_component > 0.0 {
+            linear_component.sqrt()
+        } else {
+            0.0
+        }
     }
 }
